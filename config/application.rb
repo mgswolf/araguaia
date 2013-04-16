e5092d31243c353894aa50ba58d876cd45c2f986
Marcelo Giovane Silva <mgswolf@gmail.com>
2013-04-16 12:33:29 -0300

    maintenance config

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Araguaia
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    #config.i18n.default_locale = :de
     #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.load_path += Dir[Rails.root.join("app/locales/**/*.{rb,yml}").to_s]
     config.i18n.available_locales = ['pt-BR']
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,7 @@
 # -*- encoding : utf-8 -*-
+require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+
 # General
 set :application, "arahidro"
 set :user, "railsapps"
@@ -34,7 +37,16 @@ role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
 
-# Passenger
+
+
+#before "deploy",                 "deploy:delayed_job:stop"
+#before "deploy:migrations",      "deploy:delayed_job:stop"
+
+after  "deploy:update_code",     "deploy:symlink_config"
+before "deploy:migrate",         "deploy:web:disable", "deploy:db:backup"
+
+after  "deploy",                                      "deploy:cleanup"#, "deploy:delayed_job:restart"
+after  "deploy:migrations",      "deploy:web:enable", "deploy:cleanup"#, "deploy:delayed_job:restart"
 
 namespace :deploy do
   task :start do ; end
@@ -55,8 +67,58 @@ namespace :deploy do
   end
   after "deploy:setup", "deploy:setup_config"
 
+  desc "Link in the production database.yml and assets"
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-  after "deploy:finalize_update", "deploy:symlink_config"
+
+
+
+  namespace :delayed_job do
+    desc "Restart the delayed_job process"
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec script/delayed_job restart" rescue nil
+    end
+    desc "Stop the delayed_job process"
+    task :stop, :roles => :app, :except => { :no_release => true } do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec script/delayed_job stop" rescue nil
+    end
+  end
+
+
+  namespace :db do
+    desc "backup of database before migrations are invoked"
+    task :backup, :roles => :db, :only => { :primary => true } do
+      filename = "#{deploy_to}/shared/db_backup/#{stage}_db.#{Time.now.utc.strftime("%Y-%m-%d_%I:%M")}_before_deploy.gz"
+      text = capture "cat #{deploy_to}/current/config/database.yml"
+      yaml = YAML::load(text)["#{stage}"]
+
+      on_rollback { run "rm #{filename}" }
+      run "mysqldump --single-transaction --quick -u#{yaml['username']} -h#{yaml['host']} -p#{yaml['password']} #{yaml['database']} | gzip -c > #{filename}"
+    end
+  end
+
+  namespace :web do
+    desc "Maintenance start"
+    task :disable, :roles => :web do
+      on_rollback { run "rm #{shared_path}/system/maintenance.html" }
+      page = File.read("public/503.html")
+      put page, "#{shared_path}/system/maintenance.html", :mode => 0644
+    end
+
+    desc "Maintenance stop"
+    task :enable, :roles => :web do
+      run "rm #{shared_path}/system/maintenance.html"
+    end
+  end
+end
+
+namespace :log do
+  desc "A pinch of tail"
+  task :tailf, :roles => :app do
+    run "tail -n 10000 -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
+      puts "#{data}"
+      break if stream == :err
+    end
+  end
 end
